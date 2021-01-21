530f13b6d2dc9b6e0d6162c58956e5d1bfdfceb4
NguyenKhue09 <19521712@gm.uit.edu.vn>
2021-01-22 01:06:16 +0700

    State management

---
+++ context-api-training-master/src/components/app/app.component.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import GlobalStyle from '../globalStyle'
+
+import TitleBar from '../titleBar'
+import Main from '../main'
+import Spinner from '../spinner'
+
+import LoadingProvider from '../loadingProvider'
+import UserDetailsProvider from '../userDetailsProvider'
+import Header from '../header';
+//import { ThemeProvider } from 'styled-components'
+import ThemeProvider from '../../context/change-theme'
+
+const App = () => (
+  <>
+  <ThemeProvider>
+  <Header>
+    <GlobalStyle />
+    <LoadingProvider>
+      <Spinner />
+      <UserDetailsProvider>
+        <TitleBar />
+        <Main />
+      </UserDetailsProvider>
+    </LoadingProvider>
+  </Header>
+  </ThemeProvider>
+  </>
+)
+
+export default App
